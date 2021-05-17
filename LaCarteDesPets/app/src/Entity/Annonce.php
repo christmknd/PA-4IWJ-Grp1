05e3de4f8fa874dc3f8384aaa9da4b7cef01ccfe
Maxime <maximedaude93@gmail.com>
2021-05-17 19:48:30 +0200

    [UPDATE] Basics Entity

---
+++ LaCarteDesPets/app/src/Entity/Annonce.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnnonceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnnonceRepository::class)
+ */
+class Annonce
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $etat;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $atCreated;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbrViews;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomAnimal;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $espece;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lieu;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Evenement::class, mappedBy="annonces")
+     */
+    private $evenements;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="annonces")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $utilisateur;
+
+    public function __construct()
+    {
+        $this->evenements = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEtat(): ?bool
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(bool $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+
+    public function getAtCreated(): ?\DateTimeInterface
+    {
+        return $this->atCreated;
+    }
+
+    public function setAtCreated(\DateTimeInterface $atCreated): self
+    {
+        $this->atCreated = $atCreated;
+
+        return $this;
+    }
+
+    public function getNbrViews(): ?int
+    {
+        return $this->nbrViews;
+    }
+
+    public function setNbrViews(int $nbrViews): self
+    {
+        $this->nbrViews = $nbrViews;
+
+        return $this;
+    }
+
+    public function getNomAnimal(): ?string
+    {
+        return $this->nomAnimal;
+    }
+
+    public function setNomAnimal(string $nomAnimal): self
+    {
+        $this->nomAnimal = $nomAnimal;
+
+        return $this;
+    }
+
+    public function getEspece(): ?string
+    {
+        return $this->espece;
+    }
+
+    public function setEspece(string $espece): self
+    {
+        $this->espece = $espece;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Evenement[]
+     */
+    public function getEvenements(): Collection
+    {
+        return $this->evenements;
+    }
+
+    public function addEvenement(Evenement $evenement): self
+    {
+        if (!$this->evenements->contains($evenement)) {
+            $this->evenements[] = $evenement;
+            $evenement->addAnnonce($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvenement(Evenement $evenement): self
+    {
+        if ($this->evenements->removeElement($evenement)) {
+            $evenement->removeAnnonce($this);
+        }
+
+        return $this;
+    }
+
+    public function getUtilisateur(): ?User
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(?User $utilisateur): self
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+}
