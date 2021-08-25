75b8cbbf6aab5334d81a66e2e866d8bcad123d4c
Maxime Daude <maximedaude.dev@gmail.com>
2021-08-25 18:09:06 +0200

    Mail de confirmation apres inscription OK

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -6,11 +6,16 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use App\Security\AppCustomAuthenticator;
+use App\Security\EmailVerifier;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 /**
  * @Route("/user")
@@ -32,7 +37,7 @@ class UserController extends AbstractController
     /**
      * @Route("/edit", name="user_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request): Response
+    public function edit(Request $request, EmailVerifier $emailVerifier, GuardAuthenticatorHandler $guardHandler, AppCustomAuthenticator $authenticator): Response
     {
         $user = $this->getUser();
         $email = $user->getEmail();
@@ -46,6 +51,25 @@ class UserController extends AbstractController
             // Si le mail a été modifié, le compte n'est pas vérifier
             if($email!==$form->get('email')->getData()){
                 $user->setIsVerified(false);
+
+                $email = (new TemplatedEmail())
+                    ->from(new Address('esgipa2021@gmail.com', 'Carte des Pets'))
+                    ->to($user->getEmail())
+                    ->subject('Carte des Pets - Confirmation Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+                    ->context([
+                        'user' => $user,
+                    ]);
+                // generate a signed url and email it to the user
+                $emailVerifier->sendEmailConfirmation('app_verify_email', $user, $email);
+
+                return $guardHandler->authenticateUserAndHandleSuccess(
+                    $user,
+                    $request,
+                    $authenticator,
+                    'main' // firewall name in security.yaml
+                );
+
             }
             $this->getDoctrine()->getManager()->flush();
 
